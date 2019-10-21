0f2f31051123d18136282682431bd7d0179bf627
Yeyu <ank201212x@gmail.com>
2019-10-21 21:30:32 +0800

    from git

---
+++ js/index.js
@@ -0,0 +1,24 @@
+window.onload=function(){
+  //获取dom元素
+  var arrowEl = document.querySelector("#header .header-main > .pointer");
+  var liNodes = document.querySelectorAll("#header .header-main > .nav > .list > li");
+  var upNodes = document.querySelectorAll("#header .header-main > .nav > .list > li .up");
+  var firstLiNode  = liNodes[0];
+  var firstUpNode  = firstLiNode.querySelector(".up");
+
+  firstUpNode.style.width = "100%";
+  arrowEl.style.left = firstLiNode.offsetLeft + firstLiNode.offsetWidth/2 - arrowEl.offsetWidth/2+"px";
+  for(var i=0;i<liNodes.length;i++){
+    //转绑很重要
+    liNodes[i].index = i;
+    liNodes[i].onclick=function(){
+      //i:liNodes.length 5
+      for(var i=0;i<upNodes.length;i++){
+        //upNodes[i].style.width="0";
+        upNodes[i].style.width="";
+      }
+      upNodes[this.index].style.width="100%";
+      arrowEl.style.left = liNodes[this.index].offsetLeft + liNodes[this.index].offsetWidth/2 - arrowEl.offsetWidth/2+"px";
+    }
+  }
+}
---
+++ myProject/src/js/index.js
@@ -0,0 +1,9 @@
+// import "@babel/polyfill"
+import "./frame/frame"
+import "./business/home"
+import "./business/course"
+import "./business/works"
+import "./business/about"
+import "./business/team"
+
+
